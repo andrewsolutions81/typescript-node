704263ed048c7c974cab52403aa628578cd3c509
Andres_Berrio <andrewsolutions81@gmail.com>
2022-08-12 15:01:23 -0500

    20220812_1501

--- index.ts
+++ index.ts
@@ -7,7 +7,7 @@ const app: Express = express();
 const port = process.env.PORT;
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('nice!⚡️⚡️⚡️Express + TypeScript Server⚡️⚡️⚡️');
+  res.send('nice!⚡️⚡️⚡️Express + TypeScript Server⚡️⚡️⚡️ ');
 });
 
 app.listen(port, () => {
